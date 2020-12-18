a0ebbc082c7829b86bbe7ba219a83db26e1e1192
reisa <alereis1002@gmail.com>
2020-12-18 15:03:28 +0000

    Banking App

---
+++ src/com/company/SecurityBanking.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityBanking extends Security{
+    private List<String> pins = new ArrayList();
+
+    public void addPin(String pin){
+        pins.add(pin);
+    }
+
+    public boolean login(String pinNumber) {
+        // login to check if pinNumber is correct
+
+        for(String pin: pins){
+            if(pinNumber == pin){
+                return true;
+            }
+        }
+        return false;
+    }
+}
